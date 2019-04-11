d7671bf3d28d68c4f52e50061349ecc7492cd22c
kevim <k.v.n.such@gmail.com>
2019-04-11 15:07:34 -0300

    List included

--- src/main/java/jpasqlite/Start.java
+++ src/main/java/jpasqlite/Start.java
@@ -3,6 +3,8 @@ package jpasqlite;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
 
 public class Start {
     public static void main(String... args){
@@ -17,6 +19,18 @@ public class Start {
 
         entityManager.persist( usuarioModel );
         entityManager.getTransaction().commit();
+
+
+        Query query = entityManager.createQuery("select us from UsuarioModel us",UsuarioModel.class);
+        List<UsuarioModel> usuarioModels =  query.getResultList();
+        for(UsuarioModel us:usuarioModels){
+            System.out.println("Nome: "+us.getNome());
+            System.out.println("Senha: "+us.getSenha());
+        }
+
+
+
+
         entityManager.close();
     }
 }
